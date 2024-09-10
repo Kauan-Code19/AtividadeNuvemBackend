655744648ba10361e2fd9acb56751cda66871c2e
Kauan-Code19 <pkauprofissional@gmail.com>
2024-09-10 14:08:08 -0300

    projeto criado

---
+++ src/main/java/com/kauanProjects/CRUD_AtividadeNuvem/CrudAtividadeNuvemApplication.java
@@ -0,0 +1,13 @@
+package com.kauanProjects.CRUD_AtividadeNuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAtividadeNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAtividadeNuvemApplication.class, args);
+	}
+
+}
